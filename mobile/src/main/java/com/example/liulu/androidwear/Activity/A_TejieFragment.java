c5d61bf590bf3b2c971f2bf98b7143e6295ad4c2
liulu <liuluzxc@126.com>
2015-04-21 12:05:49 +0800

    myFirst

---
+++ mobile/src/main/java/com/example/liulu/androidwear/Activity/A_TejieFragment.java
@@ -0,0 +1,27 @@
+package com.example.liulu.androidwear.Activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.liulu.androidwear.R;
+public class A_TejieFragment extends Fragment {
+
+
+    public A_TejieFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.listview_layout, container, false);
+        return v;
+    }
+
+
+}
